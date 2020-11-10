debbd561714743ba50e6063ec7aad70bcb894c5f
SamHarron <harronsam@gmail.com>
2020-11-10 11:56:04 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,14 +15,12 @@
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase == phrase.downcase
-   return "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == phrase.downcase
-   return "HUH?! SPEAK UP, SONNY!"
+
    elsif phrase == phrase.upcase
    return "NO, NOT SINCE 1938!"
-   else
-    return "NO, NOT SINCE 1938!"
-end
 
-speak_to_grandma("Hello!")
+   else
+    return "HUH?! SPEAK UP, SONNY!"
+
+   end
+end
